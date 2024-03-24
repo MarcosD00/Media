29360a9da0e1226082d24154b149ddf95cd1e64e
MarcosD00 <olito007.23@gmail.com>
2024-03-23 19:52:51 -0500

    fixing up my app

--- react-app/src/components/createPost/index.js
+++ react-app/src/components/createPost/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./createPost.css";
 
 import { addPost } from "../../store/posts";
