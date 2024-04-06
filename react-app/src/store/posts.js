d763785848baeaf6042174c6c995ebe0c1eb1c52
MarcosD00 <olito007.23@gmail.com>
2024-04-05 20:19:58 -0500

    cleaning

--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -48,7 +48,6 @@ export const singlePost = (postId) => async dispatch => {
 }
 
 export const addPost = (post) => async dispatch => {
-    // console.log(post)
     const res = await fetch("/api/post/new-post", {
         method: "POST",
         body: post
